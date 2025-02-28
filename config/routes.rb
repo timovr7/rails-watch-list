0806f56b2399b87d5a56b605f0e11ca5bb51d420
Timo Vromen <timovromen@gmail.com>
2025-02-28 12:58:20 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "lists#index"
-  resources :lists, only: [:index, :new, :create, :show, :destroy] do
+  resources :lists, only: [:new, :create, :show, :destroy] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
 
